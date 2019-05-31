044264b09eda29198eee816f3f6c33d5d1c46b78
OlhaLozinska <olja.lviv133@gmail.com>
2019-05-31 12:17:55 +0300

    refuctor method to find Average Time Of Reading By BookId. Now it works correctly

--- src/main/java/com/softserve/academy/dao/AuthorDaoImpl.java
+++ src/main/java/com/softserve/academy/dao/AuthorDaoImpl.java
@@ -9,21 +9,5 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Autowired
     private SessionFactory sessionFactory;
-//    @Override
-//    public void addEmployee(EmployeeEntity employee) {
-//        this.sessionFactory.getCurrentSession().save(employee);
-//    }
-//    @SuppressWarnings("unchecked")
-//    @Override
-//    public List<EmployeeEntity> getAllEmployees() {
-//        return this.sessionFactory.getCurrentSession().createQuery("from EmployeeEntity").list();
-//    }
-//    @Override
-//    public void deleteEmployee(Integer employeeId) {
-//        EmployeeEntity employee = (EmployeeEntity) sessionFactory.getCurrentSession().load(
-//            EmployeeEntity.class, employeeId);
-//        if (null != employee) {
-//            this.sessionFactory.getCurrentSession().delete(employee);
-//        }
-//    }
+
 }
