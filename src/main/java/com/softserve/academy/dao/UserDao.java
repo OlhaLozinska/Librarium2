99ce4725ab1c81d0a73cb1dd24837c6b58e415dd
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-01 00:01:39 +0300

    full documentation for DAO

--- src/main/java/com/softserve/academy/dao/UserDao.java
+++ src/main/java/com/softserve/academy/dao/UserDao.java
@@ -6,6 +6,7 @@
  * This software is the confidential and proprietary information of Softserve.
  *
  */
+
 package com.softserve.academy.dao;
 
 import com.softserve.academy.entity.Author;
@@ -13,16 +14,66 @@ import com.softserve.academy.entity.User;
 
 import java.util.List;
 
+/**
+ * Establish contract for User class CRUD operations.
+ *
+ * @author Olha Lozinska
+ * @author Volodymyr Oseredchuk
+ * @version 1.0
+ * @since 23.05.2019
+ */
 public interface UserDao {
+    /**
+     * Finds all users.
+     *
+     * @return list of users.
+     */
     List<User> getAllUsers();
 
+    /**
+     * Finds user by ID.
+     *
+     * @param id user ID.
+     * @return user.
+     */
     User getUserById(Integer id);
+
+    /**
+     * Finds user statistic average age.
+     *
+     * @return number of years.
+     */
     int getUserStatisticAverageAge();
+
+    /**
+     * Finds user average time of using library.
+     *
+     * @return number of days.
+     */
     int getUserAverageTimeOfUsingLibrary();
+
+    /**
+     * Finds user by username.
+     *
+     * @param userName username.
+     * @return user.
+     */
     User getUserByUsername(String userName);
 
+    /**
+     * Finds user average age by book ID.
+     *
+     * @param bookId book ID.
+     * @return number of years.
+     */
     int getUserAverageAgeByBookId(int bookId);
 
+    /**
+     * Finds user average age by author.
+     *
+     * @param author author.
+     * @return number of years.
+     */
     int getUserAverageAgeByAuthor(Author author);
 
 
