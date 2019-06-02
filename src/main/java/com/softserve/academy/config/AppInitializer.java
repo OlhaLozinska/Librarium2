05123a004934334f733d217e080fb39f62035bd3
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-02 11:41:15 +0300

    Add documentation for config classes

--- src/main/java/com/softserve/academy/config/AppInitializer.java
+++ src/main/java/com/softserve/academy/config/AppInitializer.java
@@ -11,19 +11,43 @@ package com.softserve.academy.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+ *
+ * Config class, loads all application configurations.
+ *
+ * @author Volodymyr Oseredchuk
+ * @version 1.0
+ * @since 29.05.2019
+ *
+ */
 public class AppInitializer extends
     AbstractAnnotationConfigDispatcherServletInitializer {
 
+    /**
+     * Set Class object for Hibernate configuration.
+     *
+     * @return array of Class objects.
+     */
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[] {HibernateConfig.class};
     }
 
+    /**
+     * Set Class object for WebMvc configuration.
+     *
+     * @return array of Class objects.
+     */
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[] {WebMvcConfig.class};
     }
 
+    /**
+     * Set servlet mappings.
+     *
+     * @return array of strings.
+     */
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
--- src/main/java/com/softserve/academy/config/WebMvcConfig.java
+++ src/main/java/com/softserve/academy/config/WebMvcConfig.java
@@ -24,11 +24,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+/**
+ *
+ * Config class, sets all WebMvc configurations.
+ *
+ * @author Volodymyr Oseredchuk
+ * @version 1.0
+ * @since 29.05.2019
+ *
+ */
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.softserve.academy"})
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    /**
+     * Sets path and extension for view pages.
+     *
+     * @return internal resource view resolver.
+     */
     @Bean
     public InternalResourceViewResolver resolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -38,6 +52,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return resolver;
     }
 
+    /**
+     * Configure resource message source.
+     *
+     * @return message source.
+     */
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource source = new ResourceBundleMessageSource();
@@ -45,6 +64,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return source;
     }
 
+    /**
+     * Creates validator for factory bean and sets message source for it.
+     *
+     * @return validator.
+     */
     @Override
     public Validator getValidator() {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
@@ -52,6 +76,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return validator;
     }
 
+    /**
+     * Add resource path to registry.
+     *
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
@@ -59,6 +87,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .addResourceLocations("/resources/");
     }
 
+    /**
+     * Add security interceptor path to registry.
+     *
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityInterceptor());
